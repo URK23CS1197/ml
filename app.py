d78e5ca8edc6c0292a6dd2763b86878836e99eb5
urk23cs1197 <niransonc@gmail.com>
2025-11-12 23:01:17 +0530

    ml

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import joblib
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+
+model=joblib.load("random_forest_model.pkl")
+
+@app.route("/predict",methods=['GET'])
+def predict():
+
+    feature=[78,113,22.4]
+
+    input=np.array(feature).reshape(1,-1)
+
+    prediction=model.predict(input)[0]
+
+    if prediction==0:
+        return jsonify({'prediction':'non-diabetic'})
+    else:
+        return jsonify({'prediction':'Diabetic'})
+    
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5000)
