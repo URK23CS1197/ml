7f4adb0caa3f3f529deb3beeb3328eb8dd434509
urk23cs1197 <niransonc@gmail.com>
2025-11-13 08:21:33 +0530

    prac

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,12 @@ FROM python:3.9-slim
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY app.py .
-COPY random_forest_model.pkl .
-
+COPY random_forest.pkl .
 
 EXPOSE 5000
 
-CMD ["python", "app.py"]
+CMD ["python","app.py"]
--- app.py
+++ app.py
@@ -5,22 +5,21 @@ import pandas as pd
 
 app=Flask(__name__)
 
-model=joblib.load("random_forest_model.pkl")
+model=joblib.load("random_forest.pkl")
 
-@app.route("/predict",methods=['GET'])
+@app.route('/predict',methods=['GET'])
 def predict():
 
-    feature=[78,113,22.4]
+    feature=[22.4,120,38]
 
-    input=np.array(feature).reshape(1,-1)
+    input=np.array(feature)[1,-1]
 
     prediction=model.predict(input)[0]
 
     if prediction==0:
-        return jsonify({'prediction':'non-diabetic'})
+        return jsonify({"prediction":"non-diabetic"})
     else:
-        return jsonify({'prediction':'Diabetic'})
+        return jsonify({"prediction":"diabetic"})
     
-
-if __name__=="__main__":
+if __name__=='__main__':
     app.run(host='0.0.0.0',port=5000)
